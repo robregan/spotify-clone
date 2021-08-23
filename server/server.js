1c88a068117ec5df3a32d172ba2d3bcb13e2e22a
rob <robert_regan86@yahoo.com>
2021-08-23 15:02:26 -0400

    removed cors

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require("dotenv").config()
 const express = require("express")
-const cors = require("cors")
+
 const bodyParser = require("body-parser")
 const lyricsFinder = require("lyrics-finder")
 const SpotifyWebApi = require("spotify-web-api-node")
@@ -10,7 +10,7 @@ const app = express()
 
 
 
-app.use(cors())
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
