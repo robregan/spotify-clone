9a9453d7a88afa1c499529dae1865e5c743c2070
rob <robert_regan86@yahoo.com>
2021-08-23 13:42:43 -0400

    changed redirect uri's

--- server/client/src/useAuth.js
+++ server/client/src/useAuth.js
@@ -8,7 +8,7 @@ export default function useAuth(code) {
 
   useEffect(() => {
     axios
-      .post("http://localhost:3001/login", {
+      .post("https://spotify-w-lyrics.herokuapp.com/login", {
         code,
       })
       .then(res => {
@@ -26,7 +26,7 @@ export default function useAuth(code) {
     if (!refreshToken || !expiresIn) return
     const interval = setInterval(() => {
       axios
-        .post("http://localhost:3001/refresh", {
+        .post("https://spotify-w-lyrics.herokuapp.com/refresh", {
           refreshToken,
         })
         .then(res => {
